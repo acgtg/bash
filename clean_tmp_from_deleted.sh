34aa398603b39afa3a798e9c35fe83f3dacf6362
Vitaly <VitalyKob@users.noreply.github.com>
2016-02-10 09:54:49 -0500

    Update clean_tmp_from_deleted.sh

--- clean_tmp_from_deleted.sh
+++ clean_tmp_from_deleted.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # One liner prints deleted files on tmp partitions and kill processes 
-# which still holding this files. As the result it frees up disk space on tmp partition
+# which still holding these files. As the result it frees up disk space on tmp partition
 
 p=$( lsof /tmp/ | grep "(deleted)" | awk '{print $2}' | uniq )
 kill $p
