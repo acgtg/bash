bdbd73d2be2b25ca5e1b58a4b6b2e59acc53f180
Vitaly <VitalyKob@users.noreply.github.com>
2016-02-22 15:53:39 -0500

    Update unmount.stale.nfs.sh

--- unmount.stale.nfs.sh
+++ unmount.stale.nfs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 l=$( df -h 2>&1 | grep Stale | awk '{print $2}' | awk -F \` '{print $2}' | awk -F \' '{print $1}' )
-
-for m in $l
-do
-umount $m
-done
+ for m in $l
+  do
+  umount -fl $m
+ done
