c829c66296a93d61925e58735a09d0e4613ef497
Vitaly <VitalyKob@users.noreply.github.com>
2016-10-14 11:14:55 -0400

    Update unmount.stale.nfs.sh

--- unmount.stale.nfs.sh
+++ unmount.stale.nfs.sh
@@ -5,3 +5,21 @@ l=$( df -h 2>&1 | grep Stale | awk '{print $2}' | awk -F \` '{print $2}' | awk -
   do
   umount -fl $m
  done
+
+
+# another way to unmount stale file systems
+# it will print stale file systems
+mount | sed -n "s/^.* on \(.*\) type nfs .*$/\1/p" | 
+while read mount_point ; do 
+  timeout 10 ls $mount_point >& /dev/null || echo "stale $mount_point" ; 
+done
+
+
+
+# put it in array
+
+arr=(`mount | sed -n "s/^.* on \(.*\) type nfs .*$/\1/p" |  while read mount_point ; do    timeout 10 ls $mount_point >& /dev/null || echo "stale $mount_point" ;  done`)
+
+# and unmount it
+
+for var in "${arr[@]}"; do umount -fl $var; done
