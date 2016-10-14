6e9d356b288c02b8a253588e3278cf0def3706e3
Vitaly <VitalyKob@users.noreply.github.com>
2016-10-14 13:11:45 -0400

    Update unmount.stale.nfs.sh

--- unmount.stale.nfs.sh
+++ unmount.stale.nfs.sh
@@ -18,7 +18,7 @@ done
 
 # put it in array
 
-arr=(`mount | sed -n "s/^.* on \(.*\) type nfs .*$/\1/p" |  while read mount_point ; do    timeout 10 ls $mount_point >& /dev/null || echo "stale $mount_point" ;  done`)
+arr=(`mount | sed -n "s/^.* on \(.*\) type nfs .*$/\1/p" |  while read mount_point ; do timeout 10 ls $mount_point >& /dev/null || echo "$mount_point" ;  done`)
 
 # and unmount it
 
