ecce9eed338d49f96e45b467f2f5455fc9dfd01b
Vitaly <VitalyKob@users.noreply.github.com>
2017-11-24 13:40:15 -0500

    Create lock_users.sh
    RedHat job template. This template locking users, backuping and removing their home directories. Provide list of users through satellite web interface.

---
+++ lock_users.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+mUser='<%= input("username") %>'
+
+if [ ! -d /home/OLD_account ]; then
+   mkdir -p /home/OLD_account
+fi
+
+mUser=("$@")
+
+lock_user()
+{
+  id ${user} > /dev/null 2>&1
+  user_exist=$?
+
+  if [ ${user_exist} == 0 ]; then
+    # lock user
+    passwd -l $user
+    # archive and remove user's home directory
+    cp -rp /home/$user /home/OLD_account/
+    tar -cpzf /home/OLD_account/$user-$(date +"%d-%m-%Y").tar.gz -P /home/OLD_account/$user
+    arch_status=$?
+    if [ ${arch_status} == 0 ]; then
+      rm -rf /home/$user
+      rm -rf /home/OLD_account/$user
+    fi
+  fi
+}
+
+for user in "${mUser[@]}"
+  do
+    lock_user ${user}
+  done
