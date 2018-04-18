8f71ad838c7e079b5d0ac485facfe009fa7f3933
Vitaly <VitalyKob@users.noreply.github.com>
2018-04-18 09:30:11 -0400

    Update ifrebootneeded.sh

--- ifrebootneeded.sh
+++ ifrebootneeded.sh
@@ -3,4 +3,4 @@
 LAST_KERNEL=$(rpm -q --last kernel | perl -pe 's/^kernel-(\S+).*/$1/' | head -1)
 CURRENT_KERNEL=$(uname -r)
 
-test $LAST_KERNEL = $CURRENT_KERNEL || echo REBOOT
+test $LAST_KERNEL = $CURRENT_KERNEL || echo echo "REBOOT in order to boot into the new kernel"
